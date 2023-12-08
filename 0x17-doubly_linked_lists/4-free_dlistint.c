2128c252260f4b3e1cf4942845059b55a09b5661
Elegant818 <giftpeteronyeche818@gmail.com>
2023-12-08 12:46:52 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees the  doubly linked list
+ * @head: A pointer to the first node of the list
+ *
+ * Return: Nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+}
